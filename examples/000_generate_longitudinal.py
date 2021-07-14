f9b27df5af7124c012cdf71bb9b3e683bb8d4359
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-07-14 13:35:02 +0200

    Longitudinal plane looks preserved

--- examples/000_generate_longitudinal.py
+++ examples/000_generate_longitudinal.py
@@ -1,9 +1,13 @@
+import json
+
 import numpy as np
-import xpart as xp
 from scipy.constants import e as qe
 from scipy.constants import m_p
 from scipy.constants import c as clight
 
+import pymask as pm
+import xline as xl
+import xpart as xp
 
 rfbucket = xp.RFBucket(circumference=6911., gamma=27.6433,
                        mass=m_p, charge=qe, alpha_array=[0.003077672469],
@@ -16,5 +20,47 @@ matcher = xp.RFBucketMatcher(rfbucket=rfbucket,
 
 n_part = int(1e6)
 
-z_particle, delta_particles, _, _ = matcher.generate(macroparticlenumber=n_part)
+z_particles, delta_particles, _, _ = matcher.generate(macroparticlenumber=n_part)
+
+# Match transverse plane
+line_file = ('../../xtrack/test_data/sps_w_spacecharge/'
+             'optics_and_co_at_start_ring.json')
+
+with open(line_file, 'r') as fid:
+    ddd = json.load(fid)
+RR = np.array(ddd['RR_madx'])
+part_on_co = xl.Particles.from_dict(ddd['particle_on_madx_co'])
+
+WW, WWinv, Rot = pm.compute_linear_normal_form(RR)
+
+assert len(z_particles) == len(delta_particles)
+num_particles = len(z_particles)
+
+nemitt_x = 2e-6
+nemitt_y = 2.5e-6
+
+gemitt_x = nemitt_x/part_on_co.beta0/part_on_co.gamma0
+gemitt_y = nemitt_y/part_on_co.beta0/part_on_co.gamma0
+
+x_norm = np.sqrt(gemitt_x) * np.random.normal(size=num_particles)
+px_norm = np.sqrt(gemitt_x) * np.random.normal(size=num_particles)
+y_norm = np.sqrt(gemitt_y) * np.random.normal(size=num_particles)
+py_norm = np.sqrt(gemitt_y) * np.random.normal(size=num_particles)
+
+# Transform long. coordinates to normalized space
+XX_norm = np.dot(WWinv, np.array([np.zeros(num_particles),
+                                 np.zeros(num_particles),
+                                 np.zeros(num_particles),
+                                 np.zeros(num_particles),
+                                 z_particles,
+                                 delta_particles]))
+
+XX_norm[0, :] = x_norm
+XX_norm[1, :] = px_norm
+XX_norm[2, :] = y_norm
+XX_norm[3, :] = py_norm
+
+# Transform to physical coordinates
+XX = np.dot(WW, XX_norm)
+
 
