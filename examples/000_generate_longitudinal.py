cf7479ceaa81a6ae15e225573039c0261c4672fa
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-07-14 13:54:18 +0200

    It seems that we have a matched gaussian bunch

--- examples/000_generate_longitudinal.py
+++ examples/000_generate_longitudinal.py
@@ -9,6 +9,8 @@ import pymask as pm
 import xline as xl
 import xpart as xp
 
+sigma_z = 22.5e-2
+
 rfbucket = xp.RFBucket(circumference=6911., gamma=27.6433,
                        mass=m_p, charge=qe, alpha_array=[0.003077672469],
                        harmonic_list=[4620], voltage_list=[3e6],
@@ -16,7 +18,7 @@ rfbucket = xp.RFBucket(circumference=6911., gamma=27.6433,
 
 matcher = xp.RFBucketMatcher(rfbucket=rfbucket,
         distribution_type=xp.rfbucket_matching.ThermalDistribution,
-        sigma_z=22.5e-2)
+        sigma_z=sigma_z)
 
 n_part = int(1e6)
 
@@ -63,4 +65,27 @@ XX_norm[3, :] = py_norm
 # Transform to physical coordinates
 XX = np.dot(WW, XX_norm)
 
+part = part_on_co.copy()
+part.x += XX[0, :]
+part.px += XX[1, :]
+part.y += XX[2, :]
+part.py += XX[3, :]
+part.zeta += XX[4, :]
+part.delta += XX[5, :]
+
+y_rms = np.std(part.y)
+py_rms = np.std(part.py)
+x_rms = np.std(part.x)
+px_rms = np.std(part.px)
+delta_rms = np.std(part.delta)
+zeta_rms = np.std(part.zeta)
+
+
+assert np.isclose(zeta_rms, sigma_z, rtol=1e-3, atol=1e-15)
+assert np.isclose(x_rms,
+             np.sqrt(ddd['betx']*gemitt_x + ddd['dx']**2*delta_rms**2),
+             rtol=1e-3, atol=1e-15)
+assert np.isclose(y_rms,
+             np.sqrt(ddd['bety']*gemitt_y + ddd['dy']**2*delta_rms**2),
+             rtol=1e-3, atol=1e-15)
 
