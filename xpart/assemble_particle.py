0caadcfd09ec62605de3e2ad48e3631bbcc58800
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-10-30 22:43:29 +0200

    Working on assemble matrix

--- xpart/assemble_particle.py
+++ xpart/assemble_particle.py
@@ -1,3 +1,13 @@
+def _check_lengths(**kwargs):
+    length = None
+    for nn, xx in kwargs.items()
+        if hasattr(xx, "__iter__"):
+            if length is None:
+                length = len(xx)
+            else:
+                if length != len(xx):
+                    raise ValueError(f"invalid length len({nn})={len(xx)}")
+    return length
 
 def assemble_particles(_context=None, _buffer=None, _offset=None,
                       particle_class=xt.Particles, particle_on_co=None,
@@ -10,19 +20,54 @@ def assemble_particles(_context=None, _buffer=None, _offset=None,
     assert zeta is not None
     assert delta is not None
 
-    if (x_norm is not none and px_norm is not none
-        and y_norm is not none and py_norm is not none):
+    if (x_norm is not None or px_norm is not None
+        or y_norm is not None or py_norm is not None):
 
         assert (x is  None and px is  None
                 and y is  None and py is  None)
         mode = 'normalized'
-    elif (x is not  None and px is not  None
-          and y is not  None and py is not  None):
+    elif (x is not  None or px is not  None
+          or y is not  None or py is not  None):
         assert (x_norm is None and px_norm is None
                 and y_norm is None and py_norm is None)
         mode = 'not normalized'
-    elif (x is not  None and px is not  None
     else:
         raise ValueError('Invalid input')
 
+    if mode == 'normalized':
+
+        num_particles = _check_lengths(
+            zeta=zeta, delta=delta, x_norm=x_norm, px_norm=px_norm,
+            y_norm=y_norm, py_norm=py_norm)
+
+        WW, WWinv, Rot = compute_linear_normal_form(R_matrix)
+
+        # Transform long. coordinates to normalized space
+        XX_long = np.zeros(shape=(6, num_particle), dtype=np.float64)
+        XX_long[4, :] = zeta
+        XX_long[5, :] = delta
+
+        XX_norm = np.dot(WWinv, XX_long)
+
+        XX_norm[0, :] = x_norm
+        XX_norm[1, :] = px_norm
+        XX_norm[2, :] = y_norm
+        XX_norm[3, :] = py_norm
+
+        # Transform to physical coordinates
+        XX = np.dot(WW, XX_norm)
+
+    elif mode == 'not normalized':
+
+        num_particles = _check_lengths(
+            zeta=zeta, delta=delta, x=x, px=px,
+            y=y, py=py)
+
+        XX = np.zeros(shape=(6, num_particle), dtype=np.float64)
+        XX[0, :] = x
+        XX[1, :] = px
+        XX[2, :] = y
+        XX[3, :] = py
+        XX[4, :] = zeta
+        XX[5, :] = delta
 
