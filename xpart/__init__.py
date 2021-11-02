9c9d8ef988ed7fa4be4029fe701a4668e5e71115
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-11-02 17:16:43 +0100

    Moved pyheadtail interface from xtrack

--- xpart/__init__.py
+++ xpart/__init__.py
@@ -1,3 +1,8 @@
 from .particles import Particles, gen_local_particle_api
 from .gaussian import generate_matched_gaussian_bunch
 from .general import _pkg_root
+
+def enable_pyheadtail_interface():
+    import xtrack.pyheadtail_interface.pyhtxtparticles as pp
+    import xtrack as xt
+    xt.Particles = pp.PyHtXtParticles
