3df92e7feae2ad2b3d5c06992fd82033eb4e61c6
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-11-23 21:28:30 +0100

    A rename

--- xpart/__init__.py
+++ xpart/__init__.py
@@ -1,5 +1,5 @@
 from .particles import Particles, gen_local_particle_api, pmass
-from .gaussian import generate_matched_gaussian_bunch
+from .matched_gaussian import generate_matched_gaussian_bunch
 from .general import _pkg_root
 from .build_particles import build_particles
 from .linear_normal_form import compute_linear_normal_form
