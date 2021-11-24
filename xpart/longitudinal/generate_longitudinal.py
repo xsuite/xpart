542f2e2d9d08855128af5f6d78cfa7f651c32d67
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-11-24 15:32:16 +0100

    Working on longitudinal

--- xpart/longitudinal/generate_longitudinal.py
+++ xpart/longitudinal/generate_longitudinal.py
@@ -6,18 +6,51 @@ from .rfbucket_matching import RFBucketMatcher
 from .rfbucket_matching import ThermalDistribution
 from .rf_bucket import RFBucket
 
+def _characterize_tracker(tracker, particle_ref):
+    T_rev = line.get_length()/(particle_ref.beta0[0]*clight)
+    freq_list = []
+    lag_list_deg = []
+    voltage_list = []
+    h_list = []
+    for ee in tracker.line.elements:
+        if ee.__class__.__name__ == 'Cavity':
+            freq_list.append(ee.frequency)
+            lag_list_deg.append(ee.lag)
+            voltage_list.append(ee.voltage)
+            h_list.append(ee.frequency*T_rev)
+
+    particle_co = tracker.find_closed_orbit(particle_ref)
+
+    R_matrix = tracker.compute_one_turn_matrix_finite_differences(
+                       particle_on_co=particle_co)
+
+    eta = -R_matrix[4, 5]/line.get_length() # minus sign comes from z = s-ct
+    alpha_mom_compaction = eta + 1/particle_ref.gamma0[0]**2
+
+    dct={}
+    dct['T_rev'] = T_rev
+    dct['freq_list'] = freq_list
+    dct['lag_list_deg'] = lag_list_deg
+    dct['voltage_list'] = voltage_list
+    dct['h_list'] = h_list
+    dct['alpha_momentum_compaction'] = alpha_momentum_compaction
+    dct['eta'] = eta
+
 def generate_longitudinal_coordinates(
-                                    mass0, q0, gamma0,
-                                    num_particles,
-                                    circumference,
-                                    alpha_momentum_compaction,
-                                    rf_harmonic,
-                                    rf_voltage,
-                                    rf_phase,
+                                    tracker=None,
+                                    particle_ref=None,
+                                    mass0=None, q0=None, gamma0=None,
+                                    num_particles=None,
+                                    circumference=None,
+                                    alpha_momentum_compaction=None,
+                                    rf_harmonic=None,
+                                    rf_voltage=None,
+                                    rf_phase=None,
                                     p_increment=0.,
                                     distribution='gaussian',
                                     sigma_z=None
                                     ):
+
     if distribution != 'gaussian':
         raise NotImplementedError
 
