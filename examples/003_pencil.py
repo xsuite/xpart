19fa31b67e45817171d723a46eedda5bd0e64ac3
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-11-28 07:42:50 +0100

    Avoid pointing to pymask

--- examples/003_pencil.py
+++ examples/003_pencil.py
@@ -8,15 +8,14 @@ nemitt_x = 2.5e-6
 nemitt_y = 3e-6
 
 # Load machine model (from pymask)
-filename = ('../../lhcmask/python_examples/hl_lhc_collisions_python/'
-            + 'xsuite_lines/line_bb_for_tracking.json')
+filename = ('../../xtrack/test_data/lhc_no_bb/line_and_particle.json')
 with open(filename, 'r') as fid:
     input_data = json.load(fid)
-part_on_closed_orbit = xp.Particles.from_dict(
-                                 input_data['particle_on_tracker_co'])
+particle_sample = xp.Particles.from_dict(
+                                 input_data['particle'])
 
 
-# Vertical pencil  
+# Vertical pencil
 pencil_cut_sigmas = 6.
 pencil_dr_sigmas = 0.7
 y_in_sigmas, py_in_sigmas, r_points, theta_points = xp.generate_2D_pencil(
@@ -37,12 +36,12 @@ delta = 1e-3
 #    - transform to physical coordinates (using 1-turn matrix)
 #    - handle dispersion
 #    - center around the closed orbit
-particles = xp.build_particles(
-           R_matrix=input_data['RR_finite_diffs'], particle_ref=part_on_closed_orbit,
-           zeta=zeta, delta=delta,
-           x_norm=x_in_sigmas, px_norm=px_in_sigmas,
-           y_norm=y_in_sigmas, py_norm=py_in_sigmas,
-           scale_with_transverse_norm_emitt=(nemitt_x, nemitt_y))
+# particles = xp.build_particles(
+#            R_matrix=input_data['RR_finite_diffs'], particle_ref=part_on_closed_orbit,
+#            zeta=zeta, delta=delta,
+#            x_norm=x_in_sigmas, px_norm=px_in_sigmas,
+#            y_norm=y_in_sigmas, py_norm=py_in_sigmas,
+#            scale_with_transverse_norm_emitt=(nemitt_x, nemitt_y))
 
 
 import matplotlib.pyplot as plt
