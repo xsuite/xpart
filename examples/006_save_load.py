71d2c44e92d1baa9ef501a73f963d4547c82dfec
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-12-02 18:29:12 +0100

    Save load examples

---
+++ examples/006_save_load.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+import xobjects as xo
+import xpart as xp
+
+# Create a Particles on your selected context (default is CPU)
+context = xo.ContextCupy()
+part = xp.Particles(_context=context, x=[1,2,3])
+
+########
+# JSON #
+########
+
+# Save particles to json
+import json
+with open('part.json', 'w') as fid:
+    json.dump(part.to_dict(), fid, cls=xo.JEncoder)
+
+# Load particles from json file to selected context
+with open('part.json', 'r') as fid:
+    part_from_json= xp.Particles.from_dict(json.load(fid), _context=context)
+
+##########
+# PICKLE #
+##########
+
+# Save particles to pickle file
+import pickle
+with open('part.pkl', 'wb') as fid:
+    pickle.dump(part.to_dict(), fid)
+
+# Load particles from json to selected context
+with open('part.pkl', 'rb') as fid:
+    part_from_pkl= xp.Particles.from_dict(pickle.load(fid), _context=context)
+
+#!end-doc-part
+dct = part.to_dict()
+assert isinstance(dct['delta'], np.ndarray)
+
+##############
+# PANDAS/HDF #
+##############
+
+# Save particles to hdf file via pandas
+import pandas as pd
+df = part.to_pandas()
+df.to_hdf('part.hdf', key='df', mode='w')
+
+part_from_pdhdf = xp.Particles.from_pandas(pd.read_hdf('part.hdf'))
