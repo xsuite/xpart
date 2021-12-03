e6a7f10fd72de8619efee2d56a5c9823f026672f
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-12-03 18:31:08 +0100

    Status

--- examples/particles_generation/001a_build_particles_set.py
+++ examples/particles_generation/001a_build_particles_set.py
@@ -1,7 +1,11 @@
 import xpart as xp
+import xobjects as xo
 
-# Build a reference particles
+# Build a reference particle
 p0 = xp.Particles(mass0=xp.PROTON_MASS_EV, q0=1, p0c=7e12, x=1, y=3)
+
+# Choose a context
+ctx = xo.ContextCpu()
 
 # Built a set of three particles with different x coordinates
 particles = xp.build_particles(particle_ref=p0, y=[1,2,3])
