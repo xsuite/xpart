ed00927ba1092006f8b42f363c60a9d442fd432c
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-12-03 22:33:09 +0100

    Basic example for Particles.merge

---
+++ examples/merge_and_filter/000_merge.py
@@ -0,0 +1,9 @@
+import xpart as xp
+
+p1 = xp.Particles(x=[1,2,3])
+p2 = xp.Particles(x=[4, 5])
+p3 = xp.Particles(x=6)
+
+particles = xp.Particles.merge([p1,p2,p3])
+
+print(particles.x) # gives [1. 2. 3. 4. 5. 6.] 
