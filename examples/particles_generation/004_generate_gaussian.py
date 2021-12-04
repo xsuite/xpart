8bc3e805f3365e3cfa363ef1772e29fbc7bc177c
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-12-04 10:28:51 +0100

    Update gaussian gen example

--- examples/particles_generation/004_generate_gaussian.py
+++ examples/particles_generation/004_generate_gaussian.py
@@ -9,7 +9,7 @@ import xtrack as xt
 
 bunch_intensity = 1e11
 sigma_z = 22.5e-2
-n_part = int(5e6)
+n_part = int(5e5)
 nemitt_x = 2e-6
 nemitt_y = 2.5e-6
 
@@ -21,7 +21,7 @@ tracker = xt.Tracker(line=xt.Line.from_dict(ddd['line']))
 part_ref = xp.Particles.from_dict(ddd['particle'])
 
 
-part = xp.generate_matched_gaussian_bunch(
+particles = xp.generate_matched_gaussian_bunch(
          num_particles=n_part, total_intensity_particles=bunch_intensity,
          nemitt_x=nemitt_x, nemitt_y=nemitt_y, sigma_z=sigma_z,
          particle_ref=part_ref,
@@ -32,12 +32,12 @@ part = xp.generate_matched_gaussian_bunch(
 
 # CHECKS
 
-y_rms = np.std(part.y)
-py_rms = np.std(part.py)
-x_rms = np.std(part.x)
-px_rms = np.std(part.px)
-delta_rms = np.std(part.delta)
-zeta_rms = np.std(part.zeta)
+y_rms = np.std(particles.y)
+py_rms = np.std(particles.py)
+x_rms = np.std(particles.x)
+px_rms = np.std(particles.px)
+delta_rms = np.std(particles.delta)
+zeta_rms = np.std(particles.zeta)
 
 
 fopt = ('../../../xtrack/test_data/sps_w_spacecharge/'
@@ -46,11 +46,29 @@ with open(fopt, 'r') as fid:
     dopt = json.load(fid)
 gemitt_x = nemitt_x/part_ref.beta0/part_ref.gamma0
 gemitt_y = nemitt_y/part_ref.beta0/part_ref.gamma0
-assert np.isclose(zeta_rms, sigma_z, rtol=1e-3, atol=1e-15)
+assert np.isclose(zeta_rms, sigma_z, rtol=1e-2, atol=1e-15)
 assert np.isclose(x_rms,
              np.sqrt(dopt['betx']*gemitt_x + dopt['dx']**2*delta_rms**2),
-             rtol=1e-3, atol=1e-15)
+             rtol=1e-2, atol=1e-15)
 assert np.isclose(y_rms,
              np.sqrt(dopt['bety']*gemitt_y + dopt['dy']**2*delta_rms**2),
-             rtol=1e-3, atol=1e-15)
+             rtol=1e-2, atol=1e-15)
 
+import matplotlib.pyplot as plt
+plt.close('all')
+fig1 = plt.figure(1, figsize=(6.4, 7))
+ax21 = fig1.add_subplot(3,1,1)
+ax22 = fig1.add_subplot(3,1,2)
+ax23 = fig1.add_subplot(3,1,3)
+ax21.plot(particles.x*1000, particles.px, '.', markersize=1)
+ax21.set_xlabel(r'x [mm]')
+ax21.set_ylabel(r'px [-]')
+ax22.plot(particles.y*1000, particles.py, '.', markersize=1)
+ax22.set_xlabel(r'y [mm]')
+ax22.set_ylabel(r'py [-]')
+ax23.plot(particles.zeta, particles.delta*1000, '.', markersize=1)
+ax23.set_xlabel(r'z [-]')
+ax23.set_ylabel(r'$\delta$ [1e-3]')
+fig1.subplots_adjust(bottom=.08, top=.93, hspace=.33, left=.18,
+                     right=.96, wspace=.33)
+plt.show()
