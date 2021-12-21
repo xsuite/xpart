ecb8c3eea56fe03fb82db320e2aa95715d00ab46
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-12-21 13:52:00 +0100

    Adapt to pyopencl

--- examples/delta_setitem/001_check_setitem.py
+++ examples/delta_setitem/001_check_setitem.py
@@ -4,13 +4,13 @@ import xpart as xp
 import xobjects as xo
 
 context = xo.ContextPyopencl()
-
+ctx2np = context.nparray_from_context_array
 particles = xp.Particles(_context=context, p0c=26e9, delta=[1,2,3])
 
-assert particles.delta[2] == 3
-assert np.isclose(particles.rvv[2], 1.00061, rtol=0, atol=1e-5)
-assert np.isclose(particles.rpp[2], 0.25, rtol=0, atol=1e-10)
-assert np.isclose(particles.psigma[2], 3.001464, rtol=0, atol=1e-6)
+assert ctx2np(particles.delta[2]) == 3
+assert np.isclose(ctx2np(particles.rvv[2]), 1.00061, rtol=0, atol=1e-5)
+assert np.isclose(ctx2np(particles.rpp[2]), 0.25, rtol=0, atol=1e-10)
+assert np.isclose(ctx2np(particles.psigma[2]), 3.001464, rtol=0, atol=1e-6)
 
 particles.delta[1] = particles.delta[2]
 
