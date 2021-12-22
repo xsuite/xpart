15d0ec498dd84e5d0c8644beab8c8532620591c3
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-12-22 10:44:52 -0300

    Add test

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -16,3 +16,24 @@ def test_basics():
         assert dct['psigma'][0] == 0
         assert np.isclose(dct['psigma'][1], 1e-4, rtol=0, atol=1e-9)
         assert np.isclose(1/(dct['rpp'][1]) - 1, 1e-4, rtol=0, atol=1e-14)
+
+def test_linked_arrays():
+
+    for context in xo.context.get_test_contexts():
+        print(f"Test {context.__class__}")
+
+        ctx2np = context.nparray_from_context_array
+        particles = xp.Particles(_context=context, p0c=26e9, delta=[1,2,3])
+
+        assert ctx2np(particles.delta[2]) == 3
+        assert np.isclose(ctx2np(particles.rvv[2]), 1.00061, rtol=0, atol=1e-5)
+        assert np.isclose(ctx2np(particles.rpp[2]), 0.25, rtol=0, atol=1e-10)
+        assert np.isclose(ctx2np(particles.psigma[2]), 3.001464, rtol=0, atol=1e-6)
+
+        particles.delta[1] = particles.delta[2]
+
+        assert particles.delta[2] == particles.delta[1]
+        assert particles.psigma[2] == particles.psigma[1]
+        assert particles.rpp[2] == particles.rpp[1]
+        assert particles.rvv[2] == particles.rvv[1]
+
