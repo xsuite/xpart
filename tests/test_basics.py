c72c0b3b05fa920f48f68c6d3e245d33988db5ca
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-01-19 17:56:45 +0100

    Introduced test that highlights the bug

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -17,6 +17,23 @@ def test_basics():
         assert np.isclose(dct['psigma'][1], 1e-4, rtol=0, atol=1e-9)
         assert np.isclose(1/(dct['rpp'][1]) - 1, 1e-4, rtol=0, atol=1e-14)
 
+def test_unallocated_particles():
+
+    for context in xo.context.get_test_contexts():
+        print(f"Test {context.__class__}")
+
+        particles = xp.Particles(_context=context, _capacity=10,
+        mass0=xp.PROTON_MASS_EV, q0=1, p0c=7e12, # 7 TeV
+        x=[1e-3, 0], px=[1e-6, -1e-6], y=[0, 1e-3], py=[2e-6, 0],
+        zeta=[1e-2, 2e-2], delta=[0, 1e-4])
+
+        dct = particles.to_dict() # transfers it to cpu
+        assert dct['x'][0] == 1e-3
+        assert dct['psigma'][0] == 0
+        assert np.isclose(dct['psigma'][1], 1e-4, rtol=0, atol=1e-9)
+        assert np.isclose(1/(dct['rpp'][1]) - 1, 1e-4, rtol=0, atol=1e-14)
+
+
 def test_linked_arrays():
 
     for context in xo.context.get_test_contexts():
