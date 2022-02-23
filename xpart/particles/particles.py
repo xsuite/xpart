b726fdaca5e0758ec723f6488f2d982074911576
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-02-23 19:16:49 +0100

    Change add to energy

--- xpart/particles/particles.py
+++ xpart/particles/particles.py
@@ -792,29 +792,20 @@ double LocalParticle_get_energy0(LocalParticle* part){
 /*gpufun*/
 void LocalParticle_add_to_energy(LocalParticle* part, double delta_energy, int pz_only ){
 
+    double const psigma = LocalParticle_get_psigma(part);
     double const beta0 = LocalParticle_get_beta0(part);
-    double const delta_beta0 = LocalParticle_get_delta(part) * beta0;
+    double const p0c = LocalParticle_get_p0c(part);
 
-    double const ptau_beta0 =
-        delta_energy / LocalParticle_get_energy0(part) +
-        sqrt( delta_beta0 * delta_beta0 + 2.0 * delta_beta0 * beta0
-                + 1. ) - 1.;
+    double ptau = psigma * beta0;
 
-    double const ptau   = ptau_beta0 / beta0;
-    double const psigma = ptau / beta0;
-    double const delta = sqrt( ptau * ptau + 2. * psigma + 1 ) - 1;
+    ptau += delta_energy/p0c;
 
-    double const one_plus_delta = delta + 1.;
-    double const rvv = one_plus_delta / ( 1. + ptau_beta0 );
+    double const irpp = sqrt(ptau*ptau + 2*ptau/beta0 +1);
 
-    LocalParticle_set_delta(part, delta );
-    LocalParticle_set_psigma(part, psigma );
-    LocalParticle_scale_zeta(part,
-        rvv / LocalParticle_get_rvv(part));
-
-    LocalParticle_set_rvv(part, rvv );
-
-    double const new_rpp = 1. / one_plus_delta;
+    double const new_rpp = 1./irpp;
+    LocalParticle_set_delta(part, irpp - 1.);
+    LocalParticle_set_rvv(part, irpp/(1 + beta0*ptau));
+    LocalParticle_set_psigma(part, ptau/beta0);
 
     if (!pz_only) {
         double const old_rpp = LocalParticle_get_rpp(part);
@@ -822,7 +813,6 @@ void LocalParticle_add_to_energy(LocalParticle* part, double delta_energy, int p
         LocalParticle_scale_px(part, f);
         LocalParticle_scale_py(part, f);
     }
-
     LocalParticle_set_rpp(part, new_rpp );
 }
 
