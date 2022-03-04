347851d4281c1e4ce6fe0d766a805be549cd6fc3
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-03-04 11:39:50 +0100

    This works

--- examples/particles_generation/006_match_at_element.py
+++ examples/particles_generation/006_match_at_element.py
@@ -13,6 +13,7 @@ with open(filename, 'r') as fid:
     input_data = json.load(fid)
 tracker = xt.Tracker(_context=ctx, line=xt.Line.from_dict(input_data['line']),
                      reset_s_at_end_turn=False)
+assert not tracker.iscollective
 tracker.line.particle_ref = xp.Particles.from_dict(input_data['particle'])
 
 r_sigma = 1
@@ -42,3 +43,59 @@ tw0 = tracker.twiss(at_elements=[0])
 assert np.isclose(
     np.sqrt(tw0['betx'][0]*2.5e-6/particles.beta0[0]/particles.gamma0[0]),
     np.max(np.abs(particles.x - np.mean(particles.x))), rtol=2e-3, atol=0)
+
+# Check multiple turns
+at_element = 'ip2'
+particles = xp.build_particles(tracker=tracker,
+                   x_norm=r_sigma*np.cos(theta), px_norm=r_sigma*np.sin(theta),
+                   scale_with_transverse_norm_emitt=(2.5e-6, 2.5e-6),
+                   at_element=at_element)
+
+tw = tracker.twiss(at_elements=[at_element])
+
+assert np.isclose(
+    np.sqrt(tw['betx'][0]*2.5e-6/particles.beta0[0]/particles.gamma0[0]),
+    np.max(np.abs(particles.x - np.mean(particles.x))), rtol=1e-3, atol=0)
+
+tracker.track(particles, num_turns=3,
+             ele_start=particles.at_element[0])
+
+tw0 = tracker.twiss(at_elements=[0])
+assert np.isclose(
+    np.sqrt(tw0['betx'][0]*2.5e-6/particles.beta0[0]/particles.gamma0[0]),
+    np.max(np.abs(particles.x - np.mean(particles.x))), rtol=2e-3, atol=0)
+assert np.all(particles.at_turn==3)
+assert np.allclose(particles.s, 3*tracker.line.get_length(), rtol=0, atol=1e-7)
+
+# Check collective case
+line_w_collective = xt.Line.from_dict(input_data['line'])
+for ip in range(8):
+    line_w_collective.element_dict[f'ip{ip+1}'].iscollective = True
+tracker = xt.Tracker(_context=ctx, line=line_w_collective,
+                     reset_s_at_end_turn=False)
+assert tracker.iscollective
+tracker.line.particle_ref = xp.Particles.from_dict(input_data['particle'])
+assert len(tracker._parts) == 16
+
+
+at_element = 'ip2'
+particles = xp.build_particles(tracker=tracker,
+                   x_norm=r_sigma*np.cos(theta), px_norm=r_sigma*np.sin(theta),
+                   scale_with_transverse_norm_emitt=(2.5e-6, 2.5e-6),
+                   at_element=at_element)
+
+tw = tracker.twiss(at_elements=[at_element])
+
+assert np.isclose(
+    np.sqrt(tw['betx'][0]*2.5e-6/particles.beta0[0]/particles.gamma0[0]),
+    np.max(np.abs(particles.x - np.mean(particles.x))), rtol=1e-3, atol=0)
+import pdb; pdb.set_trace()
+tracker.track(particles, num_turns=3,
+             ele_start=particles.at_element[0])
+
+tw0 = tracker.twiss(at_elements=[0])
+assert np.isclose(
+    np.sqrt(tw0['betx'][0]*2.5e-6/particles.beta0[0]/particles.gamma0[0]),
+    np.max(np.abs(particles.x - np.mean(particles.x))), rtol=2e-3, atol=0)
+assert np.all(particles.at_turn==3)
+assert np.allclose(particles.s, 3*tracker.line.get_length(), rtol=0, atol=1e-7)
