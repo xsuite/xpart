6b9b30db3793772699c0d7ebc8fc72c1dbb3480f
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-03-04 16:09:49 +0100

    Set start_tracking_at_element

--- xpart/build_particles.py
+++ xpart/build_particles.py
@@ -335,5 +335,6 @@ def build_particles(_context=None, _buffer=None, _offset=None, _capacity=None,
         assert particle_on_co.at_element[0] == at_element
         particles.s[:num_particles] = particle_on_co.s[0]
         particles.at_element[:num_particles] = at_element
+        particles.start_tracking_at_element = at_element
 
     return particles
