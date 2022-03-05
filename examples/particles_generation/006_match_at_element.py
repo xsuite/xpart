058d8401d16925c45bc9fb8172b435297a7a2826
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-03-05 15:05:29 +0100

    Example works on pyopencl

--- examples/particles_generation/006_match_at_element.py
+++ examples/particles_generation/006_match_at_element.py
@@ -6,6 +6,7 @@ import xtrack as xt
 import xobjects as xo
 
 ctx = xo.context_default
+ctx = xo.ContextPyopencl()
 
 # Load machine model (from pymask)
 filename = ('../../../xtrack/test_data/lhc_no_bb/line_and_particle.json')
@@ -21,16 +22,18 @@ r_sigma = 1
 theta = np.linspace(0, 2*np.pi, 1000)
 
 at_element = 'ip2'
-particles = xp.build_particles(tracker=tracker,
+particles = xp.build_particles(tracker=tracker, _context=ctx,
                    x_norm=r_sigma*np.cos(theta), px_norm=r_sigma*np.sin(theta),
                    scale_with_transverse_norm_emitt=(2.5e-6, 2.5e-6),
                    at_element=at_element)
 
 tw = tracker.twiss(at_elements=[at_element])
 
+particles._move_to(_context=xo.context_default) # To easily do the checks with numpy
 assert np.isclose(
     np.sqrt(tw['betx'][0]*2.5e-6/particles.beta0[0]/particles.gamma0[0]),
     np.max(np.abs(particles.x - np.mean(particles.x))), rtol=1e-3, atol=0)
+particles._move_to(_context=ctx)
 
 # Check that tracking starts from the right place
 tracker.track(particles, turn_by_turn_monitor='ONE_TURN_EBE')
@@ -42,26 +45,30 @@ assert np.all(mon.at_element[:, -1] == len(tracker.line.element_names) -1)
 
 # Check that distribution is matched at the end of the turn
 tw0 = tracker.twiss(at_elements=[0])
+particles._move_to(_context=xo.context_default)
 assert np.isclose(
     np.sqrt(tw0['betx'][0]*2.5e-6/particles.beta0[0]/particles.gamma0[0]),
     np.max(np.abs(particles.x - np.mean(particles.x))), rtol=2e-3, atol=0)
 
 # Check multiple turns
 at_element = 'ip2'
-particles = xp.build_particles(tracker=tracker,
+particles = xp.build_particles(tracker=tracker, _context=ctx,
                    x_norm=r_sigma*np.cos(theta), px_norm=r_sigma*np.sin(theta),
                    scale_with_transverse_norm_emitt=(2.5e-6, 2.5e-6),
                    at_element=at_element)
 
 tw = tracker.twiss(at_elements=[at_element])
 
+particles._move_to(_context=xo.context_default)
 assert np.isclose(
     np.sqrt(tw['betx'][0]*2.5e-6/particles.beta0[0]/particles.gamma0[0]),
     np.max(np.abs(particles.x - np.mean(particles.x))), rtol=1e-3, atol=0)
+particles._move_to(_context=ctx)
 
 tracker.track(particles, num_turns=3)
 
 tw0 = tracker.twiss(at_elements=[0])
+particles._move_to(_context=xo.context_default)
 assert np.isclose(
     np.sqrt(tw0['betx'][0]*2.5e-6/particles.beta0[0]/particles.gamma0[0]),
     np.max(np.abs(particles.x - np.mean(particles.x))), rtol=2e-3, atol=0)
@@ -69,7 +76,8 @@ assert np.all(particles.at_turn==3)
 assert np.allclose(particles.s, 3*tracker.line.get_length(), rtol=0, atol=1e-7)
 
 # Check collective case
-line_w_collective = xt.Line.from_dict(input_data['line'])
+import pdb; pdb.set_trace()
+line_w_collective = xt.Line.from_dict(input_data['line'], _context=ctx)
 for ip in range(8):
     line_w_collective.element_dict[f'ip{ip+1}'].iscollective = True
 tracker = xt.Tracker(_context=ctx, line=line_w_collective,
@@ -79,20 +87,23 @@ tracker.line.particle_ref = xp.Particles.from_dict(input_data['particle'])
 assert len(tracker._parts) == 16
 
 at_element = 'ip2'
-particles = xp.build_particles(tracker=tracker,
+particles = xp.build_particles(tracker=tracker, _context=ctx,
                    x_norm=r_sigma*np.cos(theta), px_norm=r_sigma*np.sin(theta),
                    scale_with_transverse_norm_emitt=(2.5e-6, 2.5e-6),
                    at_element=at_element)
 
 tw = tracker.twiss(at_elements=[at_element])
 
+particles._move_to(_context=xo.context_default)
 assert np.isclose(
     np.sqrt(tw['betx'][0]*2.5e-6/particles.beta0[0]/particles.gamma0[0]),
     np.max(np.abs(particles.x - np.mean(particles.x))), rtol=1e-3, atol=0)
+particles._move_to(_context=ctx)
 
 tracker.track(particles, num_turns=3)
 
 tw0 = tracker.twiss(at_elements=[0])
+particles._move_to(_context=xo.context_default)
 assert np.isclose(
     np.sqrt(tw0['betx'][0]*2.5e-6/particles.beta0[0]/particles.gamma0[0]),
     np.max(np.abs(particles.x - np.mean(particles.x))), rtol=2e-3, atol=0)
@@ -101,7 +112,7 @@ assert np.allclose(particles.s, 3*tracker.line.get_length(), rtol=0, atol=1e-7)
 
 # Check match_at_s
 at_element = 'ip6'
-particles = xp.build_particles(tracker=tracker,
+particles = xp.build_particles(tracker=tracker, _context=ctx,
                    x_norm=r_sigma*np.cos(theta), px_norm=r_sigma*np.sin(theta),
                    scale_with_transverse_norm_emitt=(2.5e-6, 2.5e-6),
                    at_element=at_element,
@@ -110,6 +121,7 @@ particles = xp.build_particles(tracker=tracker,
 
 tw = tracker.twiss(at_elements=[at_element])
 
+particles._move_to(_context=xo.context_default)
 assert np.isclose(
     np.sqrt(tw['betx'][0]*2.5e-6/particles.beta0[0]/particles.gamma0[0]),
     np.max(np.abs(particles.x - np.mean(particles.x))), rtol=1e-3, atol=0)
@@ -124,10 +136,12 @@ mu_at_element = tracker.twiss(at_elements=[at_element])['mux'][0]
 
 assert np.isclose(phasex_first_part, (mu_at_element - mu_at_s)*2*np.pi,
                   atol=0, rtol=0.02)
+particles._move_to(_context=ctx)
 
 tracker.track(particles, num_turns=3)
 
 tw0 = tracker.twiss(at_elements=[0])
+particles._move_to(_context=xo.context_default)
 assert np.isclose(
     np.sqrt(tw0['betx'][0]*2.5e-6/particles.beta0[0]/particles.gamma0[0]),
     np.max(np.abs(particles.x - np.mean(particles.x))), rtol=2e-3, atol=0)
