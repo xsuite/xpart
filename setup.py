a20c260abb74ce9cd6ce7a28b36ba0b55ae1dc3b
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-03-21 21:33:07 +0100

    Change version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ extensions = []
 
 setup(
     name='xpart',
-    version='0.5.7',
+    version='0.5.8',
     description='Generation of Particle Ensembles',
     url='https://github.com/xsuite/xpart',
     packages=find_packages(),
