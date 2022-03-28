031fcaac91a0fa944837b718a97a5142094b94cb
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-03-28 21:52:11 +0200

    Added test1

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -17,6 +17,17 @@ def test_basics():
         assert np.isclose(dct['ptau'][1], 1e-4, rtol=0, atol=1e-9)
         assert np.isclose(1/(dct['rpp'][1]) - 1, 1e-4, rtol=0, atol=1e-14)
 
+        particles = xp.Particles(_context=context,
+                mass0=xp.PROTON_MASS_EV, q0=1, p0c=3e9,
+                x=[1e-3, 0], px=[1e-6, -1e-6], y=[0, 1e-3], py=[2e-6, 0],
+                zeta=[1e-2, 2e-2], psigma=[0, 1e-4])
+
+        dct = particles.to_dict() # transfers it to cpu
+        assert dct['x'][0] == 1e-3
+        assert np.isclose(dct['ptau'][0], 0, atol=1e-14, rtol=0)
+        assert np.isclose(dct['ptau'][1]/dct['beta0'][1], 1e-4, rtol=0, atol=1e-9)
+        assert np.isclose(dct['delta'][1], 9.99995545e-05, rtol=0, atol=1e-13)
+
 def test_unallocated_particles():
 
     for context in xo.context.get_test_contexts():
