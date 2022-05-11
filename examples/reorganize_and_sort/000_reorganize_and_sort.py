aa33d72cc0eb81ccd9df17dbe53cfda12007fd6a
giadarol <giovanni.iadarola@cern.ch>
2022-05-11 23:01:13 +0200

    Fix and example

--- examples/reorganize_and_sort/000_reorganize_and_sort.py
+++ examples/reorganize_and_sort/000_reorganize_and_sort.py
@@ -1,12 +1,43 @@
+import numpy as np
 import xpart as xp
 import xtrack as xt
 
 p = xp.Particles(x=[0, 1, 2, 3, 4, 5, 6], _capacity=10)
 p.state[[0,3,4]] = 0
 
-tracker = xt.Tracker(line=xt.Line())
+tracker = xt.Tracker(line=xt.Line(elements=[xt.Cavity()]))
 tracker.track(p)
 
+assert np.all(p.particle_id == np.array([6, 1, 2, 5, 4, 3, 0,
+                                  -999999999, -999999999, -999999999]))
+assert np.all(p.x == np.array([6, 1, 2, 5, 4, 3, 0,
+                                  -999999999, -999999999, -999999999]))
+assert np.all(p.state == np.array([1, 1, 1, 1, 0, 0, 0,
+                                  -999999999, -999999999, -999999999]))
+assert p._num_active_particles == 4
+assert p._num_lost_particles == 3
+
 p.sort()
+
+assert np.all(p.particle_id == np.array([1, 2, 5, 6, 0, 3, 4,
+                                  -999999999, -999999999, -999999999]))
+assert np.all(p.particle_id == np.array([1, 2, 5, 6, 0, 3, 4,
+                                  -999999999, -999999999, -999999999]))
+assert np.all(p.state == np.array([1, 1, 1, 1, 0, 0, 0,
+                                  -999999999, -999999999, -999999999]))
+assert p._num_active_particles == 4
+assert p._num_lost_particles == 3
+
+p.sort(interleave_lost_particles=True)
+
+assert np.all(p.particle_id == np.array([0, 1, 2, 3, 4, 5, 6,
+                                  -999999999, -999999999, -999999999]))
+assert np.all(p.particle_id == np.array([0, 1, 2, 3, 4, 5, 6,
+                                  -999999999, -999999999, -999999999]))
+assert np.all(p.state == np.array([0, 1, 1, 0, 0, 1, 1,
+                                  -999999999, -999999999, -999999999]))
+assert p._num_active_particles == -2
+assert p._num_lost_particles == -2
+
 
 #p.reorganize()
