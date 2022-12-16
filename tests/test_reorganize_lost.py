6a129a208d73604c74cfc09edf6ebd666e7d0e2c
giadarol <giovanni.iadarola@cern.ch>
2022-12-16 08:41:22 +0000

    Testing GPU contexts, will fail on opencl

--- tests/test_reorganize_lost.py
+++ tests/test_reorganize_lost.py
@@ -1,22 +1,29 @@
 import numpy as np
 import xpart as xp
+import xobjects as xo
 
 def test_reorganize_particles():
 
-    particles = xp.Particles(p0c=450e9, x=[1,2,3,4,5],
-                             delta=[0.1,0.2,0.3,0.4,0.5])
-    particles0 = particles.copy()
+    for context in xo.context.get_test_contexts():
+        print(f"Testing with context {context}")
 
-    assert np.all(particles.state == 1)
+        c2n = context.nparray_from_context_array
+        n2c = context.nparray_to_context_array
 
-    n_act, n_lost = particles.reorganize()
-    assert n_act == 5
+        particles = xp.Particles(p0c=450e9, x=[1,2,3,4,5],
+                                delta=[0.1,0.2,0.3,0.4,0.5])
+        particles0 = particles.copy()
 
-    particles.state = np.array([1,1,0,0,1])
-    n_act, n_lost = particles.reorganize()
-    assert n_act == 3
-    assert n_lost == 2
+        assert np.all(c2n(particles.state) == 1)
 
-    assert np.all(particles.x[0:3] == [1,2,5])
-    assert np.all(particles.delta[0:3] == [0.1,0.2,0.5])
-    assert np.all(particles.ptau[0:3] == particles0.ptau[np.array([0,1,4])])
+        n_act, n_lost = particles.reorganize()
+        assert n_act == 5
+
+        particles.state = n2c(np.array([1,1,0,0,1]))
+        n_act, n_lost = particles.reorganize()
+        assert n_act == 3
+        assert n_lost == 2
+
+        assert np.all(c2n(particles.x[0:3]) == [1,2,5])
+        assert np.all(c2n(particles.delta[0:3]) == [0.1,0.2,0.5])
+        assert np.all(c2n(particles.ptau[0:3]) == c2n(particles0.ptau)[np.array([0,1,4])])
