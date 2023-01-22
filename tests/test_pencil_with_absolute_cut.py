db97d5cf43c44ab8c03342baeba2914ddfb515bf
giadarol <giovanni.iadarola@cern.ch>
2023-01-22 15:42:47 +0100

    Check all combinations of side and plane

--- tests/test_pencil_with_absolute_cut.py
+++ tests/test_pencil_with_absolute_cut.py
@@ -4,6 +4,7 @@
 # ######################################### #
 
 import json
+from itertools import product
 import numpy as np
 
 import xpart as xp
@@ -32,79 +33,86 @@ def test_pencil_with_absolute_cut(test_context):
     at_s = tracker.line.get_s_position(at_element) + 1.
     absolute_cut = 3e-3 # position of the jaw
     pencil_dr_sigmas = 3 # width of the pencil
-    side = '+' # side of the pencil
-    plane = 'x'
 
-    # generate pencil beam in absolute coordinates
-    v_absolute, pv_absolute = xp.generate_2D_pencil_with_absolute_cut(num_particles,
-                        plane=plane, absolute_cut=absolute_cut, dr_sigmas=pencil_dr_sigmas,
-                        side=side, tracker=tracker,
-                        nemitt_x=nemitt_x, nemitt_y=nemitt_y,
-                        at_element=at_element, match_at_s=at_s)
+    for plane, side in product(['x', 'y'], ['+', '-']):
 
-    # Vertical plane: generate gaussian distribution in normalized coordinates
-    w_in_sigmas, pw_in_sigmas = xp.generate_2D_gaussian(num_particles)
+        if side == '+':
+            absolute_cut = 3e-3
+        else:
+            absolute_cut = -3e-3
 
-    # Longitudinal plane: generate gaussian distribution matched to bucket
-    zeta, delta = xp.generate_longitudinal_coordinates(
-            num_particles=num_particles, distribution='gaussian',
-            sigma_z=10e-2, tracker=tracker)
+        print(f'plane={plane} side={side}')
 
-    # Combine the three planes
-    # (the normalized coordinates in y/py are blurred in order to preserve the geometric ones)
-    if plane == 'x':
-        particles = tracker.build_particles(nemitt_x=nemitt_x, nemitt_y=nemitt_y,
-                        x=v_absolute, px=pv_absolute,
-                        y_norm=w_in_sigmas, py_norm=pw_in_sigmas,
-                        zeta=zeta, delta=delta,
-                        at_element=at_element, match_at_s=at_s)
-    elif plane == 'y':
-        particles = tracker.build_particles(nemitt_x=nemitt_x, nemitt_y=nemitt_y,
-                        x_norm=w_in_sigmas, px_norm=pw_in_sigmas,
-                        y=v_absolute, py=pv_absolute,
-                        zeta=zeta, delta=delta,
-                        at_element=at_element, match_at_s=at_s)
+        # generate pencil beam in absolute coordinates
+        v_absolute, pv_absolute = xp.generate_2D_pencil_with_absolute_cut(num_particles,
+                            plane=plane, absolute_cut=absolute_cut, dr_sigmas=pencil_dr_sigmas,
+                            side=side, tracker=tracker,
+                            nemitt_x=nemitt_x, nemitt_y=nemitt_y,
+                            at_element=at_element, match_at_s=at_s)
 
-    # Drift to at_s position for checking
-    drift_to_at_s = xt.Drift(_context=test_context,
-                    length=at_s-tracker.line.get_s_position(at_element))
-    drift_to_at_s.track(particles)
+        # Vertical plane: generate gaussian distribution in normalized coordinates
+        w_in_sigmas, pw_in_sigmas = xp.generate_2D_gaussian(num_particles)
 
-    particles.move(_context=xo.ContextCpu())
-    # Checks
+        # Longitudinal plane: generate gaussian distribution matched to bucket
+        zeta, delta = xp.generate_longitudinal_coordinates(
+                num_particles=num_particles, distribution='gaussian',
+                sigma_z=10e-2, tracker=tracker)
 
-    tw_at_s = tracker.twiss(at_s=at_s)
-    norm_coords = tw_at_s.get_normalized_coordinates(
-                    particles, nemitt_x=nemitt_x, nemitt_y=nemitt_y,
-                    _force_at_element=0)
+        # Combine the three planes
+        # (the normalized coordinates in y/py are blurred in order to preserve the geometric ones)
+        if plane == 'x':
+            particles = tracker.build_particles(nemitt_x=nemitt_x, nemitt_y=nemitt_y,
+                            x=v_absolute, px=pv_absolute,
+                            y_norm=w_in_sigmas, py_norm=pw_in_sigmas,
+                            zeta=zeta, delta=delta,
+                            at_element=at_element, match_at_s=at_s)
+        elif plane == 'y':
+            particles = tracker.build_particles(nemitt_x=nemitt_x, nemitt_y=nemitt_y,
+                            x_norm=w_in_sigmas, px_norm=pw_in_sigmas,
+                            y=v_absolute, py=pv_absolute,
+                            zeta=zeta, delta=delta,
+                            at_element=at_element, match_at_s=at_s)
 
-    v = getattr(particles, plane)
-    pv = getattr(particles, 'p'+plane)
-    betv = getattr(tw_at_s, 'bet'+plane)[0]
-    alfv = getattr(tw_at_s, 'alf'+plane)[0]
-    nemitt_v = {'x': nemitt_x, 'y': nemitt_y}[plane]
-    sigma_v = np.sqrt(betv*nemitt_v
-        /particles._xobject.beta0[0]/particles._xobject.gamma0[0])
+        # Drift to at_s position for checking
+        drift_to_at_s = xt.Drift(_context=test_context,
+                        length=at_s-tracker.line.get_s_position(at_element))
+        drift_to_at_s.track(particles)
 
-    assert(np.isclose(np.min(np.abs(v)), abs(absolute_cut), atol=1e-7))
-    assert(np.isclose(np.max(np.abs(v)), abs(absolute_cut) + sigma_v*pencil_dr_sigmas,
-        rtol=1e-3, atol=0))
+        particles.move(_context=xo.ContextCpu())
+        # Checks
 
-    i_tip = np.argmax(np.abs(v))
-    assert np.isclose(pv[i_tip]/v[i_tip], -alfv/betv, atol=5e-4)
+        tw_at_s = tracker.twiss(at_s=at_s)
+        norm_coords = tw_at_s.get_normalized_coordinates(
+                        particles, nemitt_x=nemitt_x, nemitt_y=nemitt_y,
+                        _force_at_element=0)
 
-    if side == '+':
-        assert np.all(v >= 0)
-    else:
-        assert np.all(v <= 0)
+        v = getattr(particles, plane)
+        pv = getattr(particles, 'p'+plane)
+        betv = getattr(tw_at_s, 'bet'+plane)[0]
+        alfv = getattr(tw_at_s, 'alf'+plane)[0]
+        nemitt_v = {'x': nemitt_x, 'y': nemitt_y}[plane]
+        sigma_v = np.sqrt(betv*nemitt_v
+            /particles._xobject.beta0[0]/particles._xobject.gamma0[0])
 
-    other_plane = {'x': 'y', 'y': 'x'}[plane]
-    w_norm = getattr(norm_coords, other_plane+'_norm')
-    pw_norm = getattr(norm_coords, 'p'+other_plane+'_norm')
+        assert(np.isclose(np.min(np.abs(v)), abs(absolute_cut), atol=1e-7))
+        assert(np.isclose(np.max(np.abs(v)), abs(absolute_cut) + sigma_v*pencil_dr_sigmas,
+            rtol=1e-3, atol=0))
 
-    assert np.allclose(w_in_sigmas, w_norm, 1e-12)
-    assert np.allclose(pw_in_sigmas, pw_norm, 1e-12)
+        i_tip = np.argmax(np.abs(v))
+        assert np.isclose(pv[i_tip]/v[i_tip], -alfv/betv, atol=5e-4)
 
-    assert(np.allclose(zeta, particles.zeta, atol=1e-12))
-    assert(np.allclose(delta, particles.delta, atol=1e-12))
+        if side == '+':
+            assert np.all(v >= 0)
+        else:
+            assert np.all(v <= 0)
+
+        other_plane = {'x': 'y', 'y': 'x'}[plane]
+        w_norm = getattr(norm_coords, other_plane+'_norm')
+        pw_norm = getattr(norm_coords, 'p'+other_plane+'_norm')
+
+        assert np.allclose(w_in_sigmas, w_norm, 1e-12)
+        assert np.allclose(pw_in_sigmas, pw_norm, 1e-12)
+
+        assert(np.allclose(zeta, particles.zeta, atol=1e-12))
+        assert(np.allclose(delta, particles.delta, atol=1e-12))
 
