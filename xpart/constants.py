7eb5d4d2e1ba4a3b1d72dfb6ae108deb68289f02
sterbini <guido.sterbini@cern.ch>
2023-01-31 22:25:37 +0100

    Update constants.py
    Adding  muon mass.

--- xpart/constants.py
+++ xpart/constants.py
@@ -7,3 +7,4 @@ import scipy.constants as sc
 
 PROTON_MASS_EV = sc.m_p *sc.c**2 /sc.e
 ELECTRON_MASS_EV = sc.m_e * sc.c**2 /sc.e
+MUON_MASS_EV = sc.physical_constants['muon mass'][0] * sc.c**2 /sc.e
