26cbcd42edcfe7b974c3cf5d4030ffd529626d22
Frederik Van der Veken <frederik@cern.ch>
2023-02-13 20:20:39 +0100

    Moved sampling functions that depend on RandomGenerator to its source file, to avoid the need for BeamElements to depend on RandomGenerator when using only simple random distributions.

--- tests/test_random_gen_gauss.py
+++ tests/test_random_gen_gauss.py
@@ -34,7 +34,7 @@ def test_random_generation():
                     void TestElement_track_local_particle(
                             TestElementData el, LocalParticle* part0){
                         //start_per_particle_block (part0->part)
-                            double rr = LocalParticle_generate_random_double_gauss(part);
+                            double rr = RandomGenerator_get_double_gauss(part);
                             LocalParticle_set_x(part, rr);
                         //end_per_particle_block
                     }
