4823f7eb096157ae163d528a46c07d7d6396c427
lgiacome <lorenzo.giacome@cern.ch>
2023-11-09 11:06:14 +0100

    updated filling scheme test

--- tests/test_filling_scheme.py
+++ tests/test_filling_scheme.py
@@ -5,11 +5,15 @@ import json
 
 
 class DummyCommunicator:
-    def __init__(self, n_procs):
+    def __init__(self, n_procs, rank):
         self.n_procs = n_procs
+        self.rank = rank
 
     def Get_size(self):
         return self.n_procs
+
+    def Get_rank(self):
+        return self.rank
 
 
 def test_filling_scheme():
@@ -17,16 +21,14 @@ def test_filling_scheme():
     with open(filename, 'r') as fid:
         input_data = json.load(fid)
 
-    bunch_spacing_in_buckets = 10
     filling_scheme_array = np.zeros(3564)
-    filling_scheme_array[0:100] = 1
     n_bunches = 100
-    filled_slots = np.linspace(0, n_bunches, n_bunches)
+    filling_scheme_array[0:n_bunches] = 1
     n_procs = 3
-    communicator = DummyCommunicator(n_procs)
-    filling_scheme = xp.FillingScheme(bunch_spacing_in_buckets=bunch_spacing_in_buckets,
-                                      filling_scheme_array=filling_scheme_array, communicator=communicator,
-                                      circumference=input_data['circumference'])
+    communicator = DummyCommunicator(n_procs, rank=0)
+    h_list = [35640]
+    filling_scheme = xp.FillingScheme(filling_scheme_array=filling_scheme_array, communicator=communicator,
+                                      circumference=input_data['circumference'], harmonic_list=h_list)
 
     assert (filling_scheme.bunches_per_rank[0] == np.linspace(0, 33, 34)).all()
     assert (filling_scheme.bunches_per_rank[1] == np.linspace(34, 66, 33)).all()
