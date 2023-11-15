0e68f06336f8c0219ece294ca0d41132309fa1b1
lgiacome <lorenzo.giacome@cern.ch>
2023-11-15 16:21:47 +0100

    update example removing filling scheme

--- examples/particles_generation/007a_build_multi_bunch_beam.py
+++ examples/particles_generation/007a_build_multi_bunch_beam.py
@@ -15,7 +15,8 @@ class DummyCommunicator:
     def Get_rank(self):
         return self.rank
 
-filename = xt._pkg_root.joinpath('/Users/lorenzogiacomel/xsuite/xtrack/test_data/lhc_no_bb/line_and_particle.json')
+
+filename = xt._pkg_root.joinpath('../../../xsuite/xtrack/test_data/lhc_no_bb/line_and_particle.json')
 with open(filename, 'r') as fid:
     input_data = json.load(fid)
 
@@ -23,56 +24,69 @@ line = xt.Line.from_dict(input_data['line'])
 line.particle_ref = xp.Particles.from_dict(input_data['particle'])
 
 line.build_tracker()
-#line_dict = xp._characterize_line(line, particle_ref)
-
 circumference = line.get_length()
 h_list = [35640]
-
 bunch_spacing_in_buckets = 10
-filling_scheme_array = np.zeros(3564)
-n_bunches = 10
-filling_scheme_array[0:int(n_bunches/2)] = 1
+filling_scheme = np.zeros(int(np.amin(h_list)/bunch_spacing_in_buckets))
+n_bunches_tot = 10
+filling_scheme[0:int(n_bunches_tot/2)] = 1
 
-filling_scheme_array[n_bunches:int(3*n_bunches/2)] = 1
+filling_scheme[n_bunches_tot:int(3*n_bunches_tot/2)] = 1
 
 n_procs = 2
 rank = 0
 
 bunch_intensity = 1e11
-sigma_z = 22.5e-2/2
-n_part = int(1e4)
+sigma_z = 22.5e-2/5
+n_part_per_bunch = int(1e5)
 nemitt_x = 2e-6
 nemitt_y = 2.5e-6
 
 communicator = DummyCommunicator(n_procs, rank)
-filling_scheme = xp.FillingScheme(bunch_spacing_in_buckets=bunch_spacing_in_buckets,
-                                  filling_scheme_array=filling_scheme_array, communicator=communicator,
-                                  circumference=circumference, harmonic_list=h_list)
+
+first_bunch, n_bunches = xp.split_scheme(filling_scheme=filling_scheme,
+                                         communicator=communicator)
 
 particles = xp.generate_matched_gaussian_beam(
-         filling_scheme=filling_scheme,
-         num_particles=n_part, total_intensity_particles=bunch_intensity,
+         filling_scheme=filling_scheme,  # engine='linear',
+         num_particles=n_part_per_bunch,
+         total_intensity_particles=bunch_intensity,
          nemitt_x=nemitt_x, nemitt_y=nemitt_y, sigma_z=sigma_z,
-         line=line)
+         line=line, bunch_spacing_buckets=10,
+         first_bunch=first_bunch, n_bunches=n_bunches,
+         particle_ref=line.particle_ref
+)
 
-#assert (filling_scheme.bunches_per_rank[0] == np.linspace(0, 33, 34)).all()
-#assert (filling_scheme.bunches_per_rank[1] == np.linspace(34, 66, 33)).all()
-#assert (filling_scheme.bunches_per_rank[2] == np.linspace(67, 99, 33)).all()
+print(fr"target sigma_z: {sigma_z}")
+for i_bunch in range(n_bunches):
+    zeta_rms = np.std(particles.zeta[i_bunch*n_part_per_bunch:
+                                     (i_bunch+1)*n_part_per_bunch])
+    print(f'zeta rms of bunch {i_bunch}: {zeta_rms}')
+    assert np.isclose(zeta_rms, sigma_z, rtol=1e-2, atol=1e-15)
 
 plt.plot(particles.zeta, particles.delta, 'bx')
-rank=1
+rank = 1
 communicator = DummyCommunicator(n_procs, rank)
-filling_scheme = xp.FillingScheme(bunch_spacing_in_buckets=bunch_spacing_in_buckets,
-                                  filling_scheme_array=filling_scheme_array, communicator=communicator,
-                                  circumference=circumference, harmonic_list=h_list)
+
+first_bunch, n_bunches = xp.split_scheme(filling_scheme=filling_scheme,
+                                         communicator=communicator)
 
 particles = xp.generate_matched_gaussian_beam(
-         filling_scheme=filling_scheme,
-         num_particles=n_part, total_intensity_particles=bunch_intensity,
-         nemitt_x=nemitt_x, nemitt_y=nemitt_y, sigma_z=sigma_z,
-         line=line)
+    filling_scheme=filling_scheme,  # engine='linear',
+    num_particles=n_part_per_bunch,
+    total_intensity_particles=bunch_intensity,
+    nemitt_x=nemitt_x, nemitt_y=nemitt_y, sigma_z=sigma_z,
+    line=line, bunch_spacing_buckets=10,
+    first_bunch=first_bunch, n_bunches=n_bunches,
+    particle_ref=line.particle_ref
+)
+
+print(fr"target sigma_z: {sigma_z}")
+for i_bunch in range(n_bunches):
+    zeta_rms = np.std(particles.zeta[i_bunch*n_part_per_bunch:
+                                     (i_bunch+1)*n_part_per_bunch])
+    print(f'zeta rms of bunch {i_bunch}: {zeta_rms}')
+    assert np.isclose(zeta_rms, sigma_z, rtol=1e-2, atol=1e-15)
 
 plt.plot(particles.zeta, particles.delta, 'rx')
 plt.show()
-
-
