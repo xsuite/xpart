883e2614a1fda09ca4fe5b6c7ce4ad1266df7b1e
phbelang <philippe.belanger@cern.ch>
2024-03-23 10:48:55 +0100

    added hypersphere docs

--- xpart/transverse_generators/hypersphere.py
+++ xpart/transverse_generators/hypersphere.py
@@ -3,8 +3,32 @@ import numpy as np
 
 
 
-# Adapted from : https://baezortega.github.io/2018/10/14/hypersphere-sampling/
+
 def hypersphere(N, D, r=1, rng_seed = 0, surface=False ,unpack = False):
+    '''
+    Generate points uniformly distributed inside or on the surface of an N-dimensional hypersphere.
+    Adapted from : https://baezortega.github.io/2018/10/14/hypersphere-sampling/
+
+    Parameters
+    ----------
+    N : int
+        Number of particles to generate.
+    D : int
+        Dimension of the hypersphere.
+    r : float or list
+        Radius of the hypersphere. If a list, specifies radii for anisotropic scaling.
+    rng_seed : int
+        Seed for the random number generator for reproducibility.
+    surface : bool
+        If True, points will be generated on the surface. If False, points will be generated inside the hypersphere.
+    unpack : bool
+        If True, returns individual arrays for each dimension. If False, returns a single array with shape (N, D).
+
+    Returns
+    -------
+    samples : np.ndarray or tuple of np.ndarray
+        Generated points. Shape is (N, D) if unpack is False, otherwise D arrays of shape (N,).
+    '''
     # Set the random seed for reproducibility
     rng = np.random.default_rng(int(rng_seed))
 
@@ -37,21 +61,75 @@ def hypersphere(N, D, r=1, rng_seed = 0, surface=False ,unpack = False):
 
 
 def hypersphere_2D(num_particles,r = 1, rng_seed = 0):
-    
+    '''
+    Generate points uniformly distributed inside a 2-dimensional hypersphere (circle).
+
+    Parameters
+    ----------
+    num_particles : int
+        Number of particles to generate.
+    r : float
+        Radius of the circle.
+    rng_seed : int
+        Seed for the random number generator for reproducibility.
+
+    Returns
+    -------
+    x_norm : np.ndarray
+        x-coordinates of the generated points.
+    px_norm : np.ndarray
+        y-coordinates of the generated points.
+    '''
     x_norm , px_norm  = hypersphere(num_particles,D=2,r=r, rng_seed=rng_seed, surface = False,unpack=True)
 
     return x_norm, px_norm
 
 
 def hypersphere_4D(num_particles,rx =1,ry =1, rng_seed = 0):
-    
+    '''
+    Generate points uniformly distributed inside a 4-dimensional hypersphere with anisotropic scaling.
+
+    Parameters
+    ----------
+    num_particles : int
+        Number of particles to generate.
+    rx : float
+        Scaling factor for the x and px dimensions.
+    ry : float
+        Scaling factor for the y and py dimensions.
+    rng_seed : int
+        Seed for the random number generator for reproducibility.
+
+    Returns
+    -------
+    x_norm, px_norm, y_norm, py_norm : np.ndarray
+        Coordinates of the generated points in the 4-dimensional space.
+    '''
+
     x_norm , px_norm , y_norm, py_norm = hypersphere(num_particles,D=4,r=[rx,rx,ry,ry], rng_seed=rng_seed, surface = False,unpack=True)
 
     return x_norm , px_norm , y_norm, py_norm
 
 
 def hypersphere_6D(num_particles,rx =1,ry =1, rzeta=1, rng_seed = 0):
-    
+    '''
+    Generate points uniformly distributed inside a 6-dimensional hypersphere with anisotropic scaling.
+
+    Parameters
+    ----------
+    num_particles : int
+        Number of particles to generate.
+    rx, ry, rzeta : float
+        Scaling factors for the x, px, y, py, zeta, and pzeta dimensions respectively.
+    rng_seed : int
+        Seed for the random number generator for reproducibility.
+
+    Returns
+    -------
+    x_norm, px_norm, y_norm, py_norm, zeta_norm, pzeta_norm : np.ndarray
+        Coordinates of the generated points in the 6-dimensional space.
+    '''
+
     x_norm , px_norm , y_norm, py_norm, zeta_norm, pzeta_norm = hypersphere(num_particles,D=6,r=[rx,rx,ry,ry,rzeta,rzeta], rng_seed=rng_seed, surface = False,unpack=True)
 
     return x_norm , px_norm , y_norm, py_norm, zeta_norm, pzeta_norm
