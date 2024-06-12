742d1a136a8a9d9e61af86c74ae26282a2dbbbf4
elias <elias.waagaard@gmail.com>
2024-06-12 13:47:36 +0200

    parabolic and binomial examples

--- examples/longitudinal/006_generate_parabolic.py
+++ examples/longitudinal/006_generate_parabolic.py
@@ -11,8 +11,9 @@ import json
 import xpart as xp
 import xtrack as xt
 import xobjects as xo
+from xpart import build_particles
 
-from xpart.longitudinal import parabolic_longitudinal_distribution
+from xpart.longitudinal import generate_parabolic_longitudinal_coordinates
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -24,25 +25,40 @@ line = xt.Line.from_dict(input_data['line'])
 line.build_tracker()
 
 # Specify the beam parameters
-num_part = 1000000
+total_intensity_particles = 1e10
+num_particles = 1000000
 sigma_z = 0.05
+nemitt_x = 3e-6
+nemitt_y = 3e-6
 
 # Build a reference particle
 p0 = xp.Particles(mass0=xp.PROTON_MASS_EV, q0=1, p0c=7e12, x=1, y=3,
                   delta=[10])
 
-# Built a set of three particles with different x coordinates
-particles = parabolic_longitudinal_distribution(
-						num_particles=num_part,
-						nemitt_x=3e-6, 
-						nemitt_y=3e-6, 
-						sigma_z=sigma_z,
-						particle_ref=p0, 
-						total_intensity_particles=1e10,
-						line=line
-						)
-						
-						
+# Build Guassian normalized coordinates
+x_norm = np.random.normal(size=num_particles)
+px_norm = np.random.normal(size=num_particles)
+y_norm = np.random.normal(size=num_particles)
+py_norm = np.random.normal(size=num_particles)
+
+
+# Generate parabolic coordinates
+zeta, delta = generate_parabolic_longitudinal_coordinates(num_particles=num_particles,
+                                                     	  nemitt_x=nemitt_x, 
+                                                     	  nemitt_y=nemitt_y, 
+                                                     	  sigma_z=sigma_z,
+                                                     	  particle_ref=p0,
+                                                     	  total_intensity_particles=total_intensity_particles,
+                                                     	  line=line)
+				
+# Build particle object
+particles = build_particles(_context=None, particle_ref=p0,
+			zeta=zeta, delta=delta, 
+			x_norm=x_norm, px_norm=px_norm,
+			y_norm=y_norm, py_norm=py_norm,
+			nemitt_x=nemitt_x, nemitt_y=nemitt_y,
+			weight=total_intensity_particles/num_particles, line=line)
+
 # Make a histogram
 bin_heights, bin_borders = np.histogram(particles.zeta, bins=300)
 bin_widths = np.diff(bin_borders)
