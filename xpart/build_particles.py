478aa2983db3095b4615eedcbeb48ed87022a644
giadarol <giovanni.iadarola@cern.ch>
2025-01-23 06:42:22 +0100

    Implement include_collective

--- xpart/build_particles.py
+++ xpart/build_particles.py
@@ -57,6 +57,7 @@ def build_particles(_context=None, _buffer=None, _offset=None, _capacity=None,
                       scale_with_transverse_norm_emitt=None,
                       weight=None,
                       s_tol=1e-6,
+                      include_collective=False,
                       **kwargs, # They are passed to the twiss
                     ):
 
@@ -124,7 +125,7 @@ def build_particles(_context=None, _buffer=None, _offset=None, _capacity=None,
     zeta_norm = (zeta_norm.get() if hasattr(zeta_norm, "get") else zeta_norm)
     pzeta_norm = (pzeta_norm.get() if hasattr(pzeta_norm, "get") else pzeta_norm)
 
-    if line is not None and line.iscollective:
+    if line is not None and line.iscollective and not include_collective:
         logger.warning('Ignoring collective elements in particles generation.')
         line = line._get_non_collective_line()
 
