d6a777711821039444e4442f149eab3b6f6f276b
Frederik Van der Veken <frederik@cern.ch>
2025-03-21 09:28:51 +0100

    Changed print warning into warn(, FutureWarning).

--- xpart/pdg.py
+++ xpart/pdg.py
@@ -3,13 +3,20 @@
 # Copyright (c) CERN, 2025.                 #
 # ######################################### #
 
+from warnings import warn
+
+# This file is deprecated and will be removed in the future.
+
+
 def __getattr__(name):
     if name.startswith('__'):
         # Needed for module resolution, i.e. for the below errors to actually
         # work properly
         raise AttributeError
 
-    print("Warning: xpart.pdg moved to xtrack.particles.pdg.")
+    warn("The functionality in xpart.pdg moved to xtrack.particles.pdg. Using it "
+      + f"via xpart is still possible, but deprecated. It will be removed in the "
+      + f"future.", FutureWarning)
     import xtrack.particles.pdg as pdg
     if name in pdg.__dict__:
         return pdg.__dict__[name]
