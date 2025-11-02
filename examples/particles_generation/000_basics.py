50347f6e4bf5b6ab3ee3a5bd214aa6b6e740da96
giadarol <giovanni.iadarola@cern.ch>
2025-11-02 23:24:53 +0100

    Simplify example

--- examples/particles_generation/000_basics.py
+++ examples/particles_generation/000_basics.py
@@ -3,12 +3,10 @@
 # Copyright (c) CERN, 2021.                 #
 # ######################################### #
 
-import xpart as xp
+import xtrack as xt
 import xobjects as xo
 
-ctx = xo.ContextCpu()
-
-particles = xp.Particles(_context=ctx,
-        mass0=xp.PROTON_MASS_EV, q0=1, p0c=7e12, # 7 TeV
+particles = xt.Particles(_context=ctx,
+        mass0=xt.PROTON_MASS_EV, q0=1, p0c=7e12, # 7 TeV
         x=[1e-3, 0], px=[1e-6, -1e-6], y=[0, 1e-3], py=[2e-6, 0],
         zeta=[1e-2, 2e-2], delta=[0, 1e-4])
